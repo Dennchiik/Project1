e67880362be58cba7faa2dea7f3f375e7fc9fc27
LENOVO <LENOVO@DESKTOP-52JUCS7>
2023-04-12 19:16:19 +0300

    Добавьте файлы проекта.

---
+++ Project1/Project1.cpp
@@ -0,0 +1,12 @@
+﻿// Project1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "\n\t \"To be\n\t\tor not\n\t\t\tto be....\"\n\t\t\t\t/Shakespeare\/";
+}
+
